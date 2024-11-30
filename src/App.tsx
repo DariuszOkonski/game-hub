9c20890db7f1555598cde838ff78717e4572919c
Dariusz Okoński <darek200180@gmail.com>
2024-11-30 10:51:19 +0100

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 // https://www.chakra-ui.com/
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
-  return <Button colorScheme='red'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
